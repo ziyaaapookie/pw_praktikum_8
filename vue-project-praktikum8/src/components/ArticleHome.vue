07949076b190ef6a574cb678f3d3ce1ff6c0060a
ziyaaapookie <ziyaapookie@gmail.com>
2024-05-30 17:09:32 +0700

    Push Project Vue

---
+++ vue-project-praktikum8/src/components/ArticleHome.vue
@@ -0,0 +1,14 @@
+
+<template>
+     
+    <div>
+        <h1>Ini Adalah Komponen Artikel Home</h1>
+        <p>Ini Adalah Artikel Home</p>
+        <p>Ini Adalah Artikel Home</p>
+
+        <p>Ini Adalah Artikel Home</p>
+
+        <p>Ini Adalah Artikel Home</p>
+        
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArticleHome from '../components/ArticleHome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Zidan</h1>
+    <ArticleHome />
+  </main>
+</template>
